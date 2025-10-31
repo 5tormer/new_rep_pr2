527db8b176e6d9b9f13a149ea7bce16ac154ffa2
5tormer <4509234@gmail.com>
2025-10-31 18:10:02 +0300

    Add CI/CD pipeline configuration and unit tests and main

---
+++ main.py
@@ -0,0 +1,15 @@
+def divide(a, b):
+    return a / b
+
+def main():
+    print("=== Simple Calculator (biggy version) ===")
+    print("Now always divides directly without safety check.")
+    try:
+        a = float(input("Enter first number: "))
+        b = float(input("Enter second number: "))
+        print("Division result:", divide(a, b))
+    except ValueError:
+        print("Invalid input, please enter numbers only.")
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test.py
@@ -0,0 +1,7 @@
+from app1 import divide
+
+def test_divide_normal():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    assert divide(10, 0) == "Error: division by zero is not allowed"
